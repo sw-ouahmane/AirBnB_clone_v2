4f62bb159b780e8e639d9aa6f14088db3b5db4aa
Abdallah <sw.ouahmane@gmail.com>
2024-05-06 06:26:27 -0700

    check5

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,37 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
-"""
+"""update - uncompress and create simbolic link"""
 
-from fabric.api import put, run, env
+from fabric.api import run, put, env
 from os.path import exists
+
+env.user = 'ubuntu'
 env.hosts = ['54.237.43.101', '100.25.156.252']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """
+        Distributes an archive to your web servers
+    """
+    if not exists(archive_path):
         return False
+
+    _path = archive_path.split("/")
+    path_no_ext = _path[1].split(".")[0]
+
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        put(archive_path, "/tmp")
+        run("sudo mkdir -p /data/web_static/releases/" + path_no_ext + "/")
+        run("sudo tar -xzf /tmp/" + path_no_ext + ".tgz" +
+            " -C /data/web_static/releases/" + path_no_ext + "/")
+        run("sudo rm /tmp/" + path_no_ext + ".tgz")
+        run("sudo mv /data/web_static/releases/" + path_no_ext +
+            "/web_static/* /data/web_static/releases/" + path_no_ext + "/")
+        run("sudo rm -rf /data/web_static/releases/" +
+            path_no_ext + "/web_static")
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /data/web_static/releases/" + path_no_ext +
+            "/ /data/web_static/current")
         return True
-    except:
+
+    except Exception:
         return False

6b984dd0751668de41c9a1af4577a455b2a50cea
Abdallah <sw.ouahmane@gmail.com>
2024-05-25 20:38:14 +0100

    Check

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""test for console"""
+"""Test for console"""
 import unittest
 from unittest.mock import patch
 from io import StringIO
@@ -7,30 +7,33 @@ import os
 import console
 from console import HBNBCommand
 
-
 class TestConsole(unittest.TestCase):
     """Test Suite for the console"""
 
     @classmethod
     def setUpClass(cls):
-        """setup for the test"""
+        """Setup for the test"""
+        print("Setting up class...")
         cls.consol = HBNBCommand()
 
     @classmethod
     def teardown(cls):
-        """at the end of the test this will tear it down"""
+        """Teardown at the end of the test"""
+        print("Tearing down class...")
         del cls.consol
 
     def tearDown(self):
         """Remove temporary file (file.json) created as a result"""
-        if (os.getenv('HBNB_TYPE_STORAGE') != 'db'):
+        print("Tearing down...")
+        if os.getenv('HBNB_TYPE_STORAGE') != 'db':
             try:
                 os.remove("file.json")
             except Exception:
                 pass
 
     def test_docstrings_in_console(self):
-        """checking for docstrings"""
+        """Checking for docstrings"""
+        print("Testing docstrings...")
         self.assertIsNotNone(console.__doc__)
         self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
         self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
@@ -44,10 +47,10 @@ class TestConsole(unittest.TestCase):
 
     def test_emptyline(self):
         """Test empty line"""
+        print("Testing empty line...")
         with patch('sys.stdout', new=StringIO()) as f:
             self.consol.onecmd("\n")
             self.assertEqual('', f.getvalue())
-
 
 if __name__ == "__main__":
     unittest.main()

804e7ce02a8911ca5bbd9305445a6764a7a0a655
Abdallah <sw.ouahmane@gmail.com>
2024-05-26 01:26:41 +0100

    Add

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Test for console"""
+"""test for console"""
 import unittest
 from unittest.mock import patch
 from io import StringIO
@@ -7,29 +7,30 @@ import os
 import console
 from console import HBNBCommand
 
+
 class TestConsole(unittest.TestCase):
     """Test Suite for the console"""
 
     @classmethod
     def setUpClass(cls):
-        """Setup for the test"""
+        """setup for the test"""
         cls.consol = HBNBCommand()
 
     @classmethod
     def teardown(cls):
-        """Teardown at the end of the test"""
+        """at the end of the test this will tear it down"""
         del cls.consol
 
     def tearDown(self):
         """Remove temporary file (file.json) created as a result"""
-        if os.getenv('HBNB_TYPE_STORAGE') != 'db':
+        if (os.getenv('HBNB_TYPE_STORAGE') != 'db'):
             try:
                 os.remove("file.json")
             except Exception:
                 pass
 
     def test_docstrings_in_console(self):
-        """Checking for docstrings"""
+        """checking for docstrings"""
         self.assertIsNotNone(console.__doc__)
         self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
         self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
@@ -46,6 +47,3 @@ class TestConsole(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as f:
             self.consol.onecmd("\n")
             self.assertEqual('', f.getvalue())
-
-if __name__ == "__main__":
-    unittest.main()

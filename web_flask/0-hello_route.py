3f6168236928c3d692605b940ebe28e3f19f7777
Abdallah <sw.ouahmane@gmail.com>
2024-06-13 12:56:20 +0000

    Add

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
 """
-starts a Flask web application
+Starts a Flask web application
 """
 
 from flask import Flask
 app = Flask(__name__)
-
 
 @app.route('/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
+@app.route('/airbnb-onepage/', strict_slashes=False)
+def airbnb_onepage():
+    """returns Hello HBNB! at the /airbnb-onepage/ route"""
+    return 'Hello HBNB!'
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
+
